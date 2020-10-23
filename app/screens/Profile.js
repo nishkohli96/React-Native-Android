acdc2344d026b92f626f8e2000f9bf00150463c3
nishkohli96 <nishkohli96@gmail.com>
2020-10-24 02:46:52 +0530

    theme change wip

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,12 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Switch } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+    ThemedView,
+    ThemedText,
+    ThemedContainer,
+} from '@styledComps/ThemedComps';
+import { CommonStyles } from '@themes/CommonStyles';
+import { ThemeContext } from '@context/ThemeContext';
 
 const Profile = () => {
+
+    const { Dark, changeTheme } = React.useContext(ThemeContext);
+    const toggleTheme = () => changeTheme();
+
     return (
-        <View>
-            <Text> Profile</Text>
-        </View>
+        <ThemedContainer>
+      
+            <ThemedView style={styles.itemView}>
+                <ThemedView style={styles.iconView}>
+                    <Icon
+                        name="palette"
+                        color="#d6bd8b"
+                        size={CommonStyles.icons.tabIcons}
+                    />
+                </ThemedView>
+                <ThemedView style={styles.itemText}>
+                    <ThemedText style={styles.text}>
+                        Dark Theme
+                    </ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.itemOption}>
+                    <Switch
+                        trackColor={{ false: '#767577', true: '#7b9ab0' }}
+                        thumbColor={Dark ? '#42a4eb' : '#f4f3f4'}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={toggleTheme}
+                        value={Dark}
+                    />
+                </ThemedView>
+            </ThemedView>
+
+        </ThemedContainer>
     );
 };
+
+
+const styles = StyleSheet.create({
+    itemView: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingTop: 25,
+        paddingBottom: 25,
+        paddingLeft: 5,
+        paddingRight: 5,
+        borderBottomColor: 'silver',
+        borderBottomWidth: 0.5,
+    },
+    iconView: {
+        display: 'flex',
+        flex: 0.2,
+        alignItems: 'center',
+    },
+    itemText: {
+        flex: 0.6,
+        paddingLeft: 20,
+    },
+    itemOption: {
+        display: 'flex',
+        flex: 0.2,
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: CommonStyles.texts.settingsText,
+    },
+});
 
 export default Profile;
